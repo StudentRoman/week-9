666de378f585a3b3f53963f34e5cb1b6feccb664
StudentRoman <143340583+StudentRoman@users.noreply.github.com>
2023-12-23 00:39:55 +0300

    docs: correct documentation

--- src/main/java/edu/penzgtu/oop/Application.java
+++ src/main/java/edu/penzgtu/oop/Application.java
@@ -9,6 +9,5 @@ public class Application {
   public static void main(String[] args) throws IOException {
     ConfigUtil.setConfig("config.properties");
     MainFrontend.show();
-    System.out.println("123");
   }
 }
